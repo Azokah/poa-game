d2ad0c38fb4fa240d2a0e0738b7dfb5a4e498b7f
Jaime Gonzalez Bonorino <jaimegbonorino@gmail.com>
2018-06-05 18:12:08 -0300

    added colisions

--- game.py
+++ game.py
@@ -13,20 +13,26 @@ MAP_H = 12
 HERO_HP_MAX = 3
 HERO_SPEED = 4
 
-
-#Cargamos el mapa desde el archivo txt
-mapa = numpy.loadtxt('resources/level1-12.txt',dtype=int,delimiter=',')
-print(mapa)
-
 #Iniciamos modulos de pygame
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
-#Cargamos las imagenes
-mapImg = [pygame.image.load('resources/floor1.png'), pygame.image.load('resources/wall.png'),
-    pygame.image.load('resources/floor2.png'), pygame.image.load('resources/floor3.png'),]
-mapImgRect = [mapImg[0].get_rect(),mapImg[1].get_rect(),mapImg[2].get_rect(),mapImg[3].get_rect()]
+#Creamos el mapa
+class Mapa:
+    def __init__(self):
+        self.mapa = numpy.loadtxt('resources/level1-12.txt',dtype=int,delimiter=',') #Cargamos el mapa desde el archivo txt
+        #Cargamos las imagenes
+        self.img = [pygame.image.load('resources/floor1.png'), pygame.image.load('resources/wall.png'),
+            pygame.image.load('resources/floor2.png'), pygame.image.load('resources/floor3.png'),]
+        self.imgRect = [self.img[0].get_rect(),self.img[1].get_rect(),self.img[2].get_rect(),self.img[3].get_rect()]
 
+    def testColision(self,X,Y):
+        X = int(X/TILE_W)
+        Y = int(Y/TILE_H)
+        if self.mapa[Y][X] == 1:
+            return False
+        return True
+mapa = Mapa()
 #Creamos al heroe
 class Heroe:
     def __init__(self):
@@ -41,22 +47,26 @@ class Heroe:
         self.gotoY = self.imgRect.y
 
     #Metodos para setear destino
-    def goTo(X,Y):
+    def goTo(self,X,Y):
         self.gotoX = X
         self.gotoY = Y
         self.moving = True
-    def moveUp(self):
-        self.gotoY -= TILE_H
-        self.moving = True
-    def moveDown(self):
-        self.gotoY += TILE_H
-        self.moving = True
-    def moveLeft(self):
-        self.gotoX -= TILE_W
-        self.moving = True
-    def moveRight(self):
-        self.gotoX += TILE_W
-        self.moving = True
+    def moveUp(self,mapa):
+        if mapa.testColision(self.gotoX,self.gotoY - TILE_H):
+            self.gotoY -= TILE_H
+            self.moving = True
+    def moveDown(self,mapa):
+        if mapa.testColision(self.gotoX,self.gotoY + TILE_H):
+            self.gotoY += TILE_H
+            self.moving = True
+    def moveLeft(self,mapa):
+        if mapa.testColision(self.gotoX - TILE_W,self.gotoY):
+            self.gotoX -= TILE_W
+            self.moving = True
+    def moveRight(self,mapa):
+        if mapa.testColision(self.gotoX + TILE_W,self.gotoY):
+            self.gotoX += TILE_W
+            self.moving = True
 
     #Metodo update del heroe
     def update(self):
@@ -76,8 +86,9 @@ class Heroe:
 
     def damage(self):
         self.hp -= 1
-            
+
 heroe = Heroe() #Instanciamos al heroe
+
 
 #El game loop simple
 while 1:
@@ -90,21 +101,25 @@ while 1:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
             if event.key == pygame.K_UP:
-                heroe.moveUp()
+                if heroe.moving == False:
+                    heroe.moveUp(mapa)
             if event.key == pygame.K_DOWN:
-                heroe.moveDown()
+                if heroe.moving == False:
+                    heroe.moveDown(mapa)
             if event.key == pygame.K_LEFT:
-                heroe.moveLeft()
+                if heroe.moving == False:
+                    heroe.moveLeft(mapa)
             if event.key == pygame.K_RIGHT:
-                heroe.moveRight()
+                if heroe.moving == False:
+                    heroe.moveRight(mapa)
 
     screen.fill(BACKGROUND_COLOR)#Pintamos pantalla de negro
     #Graficamos todo el mapa
     for w in range(0, 12):
         for  j in range(0, 12):
-                mapImgRect[mapa[j][w]].x = w*TILE_W
-                mapImgRect[mapa[j][w]].y = j*TILE_H
-                screen.blit(mapImg[mapa[j][w]], mapImgRect[mapa[j][w]])
+                mapa.imgRect[mapa.mapa[j][w]].x = w*TILE_W
+                mapa.imgRect[mapa.mapa[j][w]].y = j*TILE_H
+                screen.blit(mapa.img[mapa.mapa[j][w]], mapa.imgRect[mapa.mapa[j][w]])
     #Graficamos al heroe
     screen.blit(heroe.img, heroe.imgRect)
 
